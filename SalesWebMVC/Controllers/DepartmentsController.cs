9a3df64c111fd2eda0151e313837877215c9d90c
gtadayukey <gtadayukey@gmail.com>
2024-04-04 08:10:20 -0300

    first model-view-controller department

---
+++ SalesWebMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> departments = [];
+            departments.Add(new Department { Id = 1, Name = "Legos" });
+            departments.Add(new Department { Id = 2, Name = "Nerfs" });
+
+            return View(departments);
+        }
+    }
+}
